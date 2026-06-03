dda9d2db7a67b393ea5f304cb4cef9420c8687ac
Anay Dandekar <anaydbcp@gmail.com>
2026-06-02 20:54:15 -0400

    Created empty template classes for the text scripts for other pages

--- app/src/assets/textScripts.js
+++ app/src/assets/textScripts.js
@@ -2,8 +2,17 @@ export class HomeScripts {
     static heroTitle = "Unlock your potential.";
     static heroContent = [
         "Welcome to Fast Lit, your gateway to concise and engaging literature.",
-        "Browse our handpicked library and discover new stories in minutes.",
+        "Use our browser extension to drop in articles and stories to start reading in minutes.",
         "Whether you're looking to learn, relax, or get inspired, we make reading easy and fun."
     ].join(" ");
     static heroButton = "Start Reading";
+}
+
+export class ReadScripts {
+}
+
+export class FeedbackScripts {
+}
+
+export class ChangelogScripts {
 }
